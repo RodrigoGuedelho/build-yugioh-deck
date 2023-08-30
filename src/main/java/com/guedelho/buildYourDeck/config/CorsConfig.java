9c4d21b0c4f1bb2b2d3225497a51aa1559c45396
Rodrigo Guedelho <rodrigohmjj97@hotmail.com>
2023-08-30 13:49:55 -0300

    Criado arquivo para configuração geral do CORS. Também removida classe de teste

---
+++ src/main/java/com/guedelho/buildYourDeck/config/CorsConfig.java
@@ -0,0 +1,22 @@
+package com.guedelho.buildYourDeck.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+
+@Configuration
+@EnableWebMvc
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**") // Permite que todas as URLs tenham acesso CORS
+                .allowedOrigins("*") // Permitir solicitações de qualquer origem
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT") // Métodos HTTP permitidos
+                .allowedHeaders("*")
+                .allowCredentials(true).maxAge(3600);; // Permitir todos os cabeçalhos
+
+    }
+}
